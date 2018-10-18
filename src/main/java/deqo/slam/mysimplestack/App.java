43b297177ab0c3483b9a54554c6b11c1f4bf423e
saralamhaourak <saralamhaourak@gmail.com>
2018-10-18 10:39:17 +0200

    first commit

---
+++ src/main/java/deqo/slam/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.slam.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
